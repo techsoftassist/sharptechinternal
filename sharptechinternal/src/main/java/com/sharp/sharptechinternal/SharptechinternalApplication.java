947f2d84bcfd8f1d6e4fddbecc26dd219e0e8de7
techsoftassist <techsoftassist@gmail.com>
2024-05-16 11:43:13 +0530

    first commit
    Signed-off-by: techsoftassist <techsoftassist@gmail.com>

---
+++ sharptechinternal/src/main/java/com/sharp/sharptechinternal/SharptechinternalApplication.java
@@ -0,0 +1,13 @@
+package com.sharp.sharptechinternal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharptechinternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharptechinternalApplication.class, args);
+	}
+
+}
---
+++ sharptechinternal/src/test/java/com/sharp/sharptechinternal/SharptechinternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharp.sharptechinternal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharptechinternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
